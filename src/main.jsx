6c3559d44de4731909db91354ef74aa577995272
Prasanth <sekaranp92@gmail.com>
2023-09-27 05:59:35 -0400

    "remove test comment"

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { StockOverview } from '@routes/StockOverview';
 import { StockDetails } from '@routes/StockDetails';
 
-
+//console.log(b)
 const routes = createBrowserRouter([{
     path: "/",
     element: <App />, 
@@ -20,6 +20,7 @@ const routes = createBrowserRouter([{
     ]
 }])
 
+debugger;
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
